292b9d0e2bfe5bc39a5ac9296d4cdc3146d9b76d
usuario <usuario@DESKTOP-F8PA4A1>
2020-01-16 10:33:24 +0100

    Bola rebota en la pala

--- src/main/java/es/luisenriquechacon/pong2/App.java
+++ src/main/java/es/luisenriquechacon/pong2/App.java
@@ -1,65 +1,136 @@
 package es.luisenriquechacon.pong2;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
+    final short SCENE_HEIGHT = 480;
+    final short SCENE_WIDTH = 640;
+
+    short ballCenterX = 0;
+    byte ballCurrentSpeedX = 10;
+    byte ballDirectionX = 1;
+    
+    short ballCenterY = 0;
+    byte ballCurrentSpeedY = 10;
+    byte ballDirectionY = 1;
+       
+    short stickHeight = 50;        
+    short stickPosY = (short)((SCENE_HEIGHT-stickHeight)/2);
+    byte stickCurrentSpeed = 2;
+    byte stickDirection = 0;
+    
     @Override
     public void start(Stage stage) {
         
-        final short SCENE_HEIGHT = 480;
-        final short SCENE_WIDHT = 640;
         
- //       StackPane root = new StackPane();
+//        StackPane root = new StackPane();
         Pane root = new Pane();
-        var scene = new Scene(new StackPane (root), SCENE_WIDHT, SCENE_HEIGHT);
+        var scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
         scene.setFill(Color.BLACK);
         stage.setScene(scene);
         stage.show();
         
+        // new Circle() => Crear un objeto de la clase Circle
         Circle circleBall = new Circle();
+        // Llamando a métodos del objeto circleBall
         circleBall.setCenterX(10);
         circleBall.setCenterY(30);
-        circleBall.setRadius(7);
+        circleBall.setRadius(7);  
         circleBall.setFill(Color.WHITE);
+        
         //Circle circleBall = new Circle(10, 30, 7);
         
         root.getChildren().add(circleBall);
-        short rectHeight = 50;
         
         Rectangle rectStick = new Rectangle();
         rectStick.setWidth(10);
-        rectStick.setHeight(rectHeight);
-        rectStick.setX(SCENE_WIDHT - 40);
-        rectStick.setY((SCENE_HEIGHT-rectHeight)/2);
+        rectStick.setHeight(stickHeight);
+        rectStick.setX(SCENE_WIDTH - 40);
+        rectStick.setY(stickPosY);
         rectStick.setFill(Color.WHITE);
         
         root.getChildren().add(rectStick);
         
-        scene.setOnKeyPressed((final KeyEvent KeyEvent) -> {
-            public void handle(final KeyEvent KeyEvent)
-            switch(keyEvent.getCode()) {
-                case UP:
-                    
-                
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(final KeyEvent keyEvent) {
+                switch(keyEvent.getCode()) {
+                    case UP:
+                        stickDirection = -1;
+                        break;
+                    case DOWN:
+                        stickDirection = 1;
+                        break;
+                }                
             }
-        }
+        });
+//        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+//            public void handle(final KeyEvent keyEvent) {
+//                stickDirection = 0;
+//            }
+//        });
         
-        
-    }
+        Timeline timeline = new Timeline(
+            // 0.017 ~= 60 FPS
+            new KeyFrame(Duration.seconds(0.017), new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent ae) {
+                    circleBall.setCenterX(ballCenterX);
+                    circleBall.setCenterY(ballCenterY);
+                    ballCenterX += ballCurrentSpeedX * ballDirectionX;
+                    ballCenterY += ballCurrentSpeedY * ballDirectionY;
+                    // Control de rebote horizontal
+                    if(ballCenterX >= 640) {
+                        ballDirectionX = -1;
+                    } else if(ballCenterX <= 0){
+                        ballDirectionX = 1;
+                    }
+                    // Control de rebote vertical
+                    if(ballCenterY >= 480) {
+                        ballDirectionY = -1;
+                    } else if(ballCenterY <= 0){
+                        ballDirectionY = 1;
+                    }
+                    
+                    rectStick.setY(stickPosY);
+                    stickPosY += stickCurrentSpeed * stickDirection;
+                    if(stickPosY <= 0 || stickPosY >= SCENE_HEIGHT-stickHeight) {
+                        stickDirection = 0;
+                    }
+                    if(stickPosY <= 0) {
+                        stickDirection = 0;
+                        stickPosY = 0;
+                    } else if (stickPosY >= SCENE_HEIGHT-stickHeight) {
+                        stickDirection = 0;
+                        stickPosY = (short)(SCENE_HEIGHT-stickHeight);
+                    }
+                    
+                    Shape shapeCollision = Shape.intersect(circleBall, rectStick);
+                    boolean colisionVacia = shapeCollision.getBoundsInLocal().isEmpty();
+                    if(colisionVacia == false) {
+                        System.out.println("Ha colisionados");
+                        ballDirectionX = -1;
+                    }
+                }
+            })                
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();      
     }
 
     public static void main(String[] args) {
